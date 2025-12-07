76de9dd3bb04244688fb1c25d99c8230ffd0d666
PedroRomaoDev <pedro7ntj@gmail.com>
2025-12-07 20:17:08 -0300

    feat: add footer responsive component

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,7 +1,47 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import FooterColumn from "./FooterColumn";
+
 const Footer = () => {
   return (
-    <div className=''>Footer</div>
-  )
-}
+    <div className="md:gap-0 gap-8 mt-16 flex flex-col items-center md:flex-row md:items-start bg-gray-800 p-8 rounded-lg md:justify-between">
 
-export default Footer
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Logo text="TRENDLAMA." textClassName="text-white" />
+        <p className="text-sm text-gray-400">© 2025 Trendlama.</p>
+        <p className="text-sm text-gray-400">All rights reserved.</p>
+      </div>
+
+      <FooterColumn
+        title="Links"
+        links={[
+          { label: "Homepage", href: "/" },
+          { label: "Contact", href: "/" },
+          { label: "Terms of Service", href: "/" },
+          { label: "Privacy Policy", href: "/" },
+        ]}
+      />
+      <FooterColumn
+        title="Links"
+        links={[
+          { label: "All Products", href: "/" },
+          { label: "New Arrivals", href: "/" },
+          { label: "Best Sellers", href: "/" },
+          { label: "Sale", href: "/" },
+        ]}
+      />
+
+      <FooterColumn
+        title="Links"
+        links={[
+          { label: "About", href: "/" },
+          { label: "Contact", href: "/" },
+          { label: "Blog", href: "/" },
+          { label: "Affiliate Program", href: "/" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Footer;
