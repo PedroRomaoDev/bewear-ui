a8eedc6a0286e0cd0f2ff2c3db19cd77dbf8ee03
PedroRomaoDev <pedro7ntj@gmail.com>
2026-01-20 19:45:20 -0300

    feat(admin): add AddOrder component

--- admin/src/components/AppSidebar.tsx
+++ admin/src/components/AppSidebar.tsx
@@ -14,6 +14,7 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 
+import AddOrder from "./AddOrder";
 import EditUser from "./EditUser";
 import {
   DropdownMenu,
@@ -213,7 +214,7 @@ const AppSidebar = () => {
                         </Link>
                       </SidebarMenuButton>
                     </SheetTrigger>
-                    <EditUser />
+                    <AddOrder />
                   </Sheet>
                 </SidebarMenuButton>
               </SidebarMenuItem>
